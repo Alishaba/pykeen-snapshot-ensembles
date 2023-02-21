049fe07601c12be67e4db9411074b742cee6df05
Alishaba <Shaban-a@live.com>
2023-02-21 20:44:00 +0100

    modifying init file to aloow imports

--- src/pykeen/evaluation/__init__.py
+++ src/pykeen/evaluation/__init__.py
@@ -7,6 +7,7 @@ from class_resolver import ClassResolver
 from .classification_evaluator import ClassificationEvaluator, ClassificationMetricResults
 from .evaluation_loop import LCWAEvaluationLoop
 from .evaluator import Evaluator, MetricResults, evaluate
+from .ensemble_evaluator import EnsembleEvaluator
 from .ogb_evaluator import OGBEvaluator
 from .rank_based_evaluator import (
     MacroRankBasedEvaluator,
@@ -14,26 +15,49 @@ from .rank_based_evaluator import (
     RankBasedMetricResults,
     SampledRankBasedEvaluator,
 )
+from .rank_based_evaluator import (
+    MacroRankBasedEvaluator,
+    RankBasedEvaluator,
+    RankBasedMetricResults,
+    SampledRankBasedEvaluator,
+)
+from .ensemble_rank_based_evaluator import (
+    EnsembleMacroRankBasedEvaluator,
+    EnsembleRankBasedEvaluator,
+    EnsembleRankBasedMetricResults,
+    EnsembleSampledRankBasedEvaluator,
+)
 
 __all__ = [
     "evaluate",
     "Evaluator",
+    "EnsembleEvaluator",
     "MetricResults",
     "RankBasedEvaluator",
+    "EnsembleRankBasedEvaluator",
     "RankBasedMetricResults",
+    "EnsembleRankBasedMetricResults",
     "MacroRankBasedEvaluator",
+    "EnsembleMacroRankBasedEvaluator",
     "LCWAEvaluationLoop",
     "SampledRankBasedEvaluator",
+    "EnsembleSampledRankBasedEvaluator",
     "OGBEvaluator",
     "ClassificationEvaluator",
     "ClassificationMetricResults",
     "evaluator_resolver",
+    "ensemble_evaluator_resolver",
     "metric_resolver",
 ]
 
 evaluator_resolver: ClassResolver[Evaluator] = ClassResolver.from_subclasses(
     base=Evaluator,
     default=RankBasedEvaluator,
+)
+
+ensemble_evaluator_resolver: ClassResolver[EnsembleEvaluator] = ClassResolver.from_subclasses(
+    base=EnsembleEvaluator,
+    default=EnsembleRankBasedEvaluator,
 )
 
 metric_resolver: ClassResolver[MetricResults] = ClassResolver.from_subclasses(MetricResults)
